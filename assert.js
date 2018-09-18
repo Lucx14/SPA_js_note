79777aee99ebcd2d4c0022a053682c9b7695f3aa
Lucien Najev <luciennajev@hotmail.com>
2018-09-18 20:31:22 +0100

    added an assertion file if i later decide to tidy up my code its there as a guide

--- assert.js
+++ assert.js
@@ -1,12 +1,14 @@
 // var assert = {
 //
-//   isTrue: function(assertionToCheck) {
+//   isTrue: function(assertionToCheck, description) {
 //     if(!assertionToCheck) {
-//       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+//       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy. " + description);
+//     } else {
+//       console.log("Assertion passed: " + assertionToCheck + ". " + description);
 //     }
 //   }
 // };
-
-
+//
+//
 //  An assertion file can be used to support the test file and make it more readable
-//  To think about
+//
