a158f3cb1e770eb5c388c77b1f42efd74c419a22
Lucien Najev <luciennajev@hotmail.com>
2018-09-19 10:19:00 +0100

    added a note-controller

---
+++ src/note-controller.js
@@ -0,0 +1,19 @@
+// (function(exports) {
+//
+//   function NoteController() {
+
+    // var element = document.getElementById("app");
+    // element.innerHTML = "Bye";
+    // console.log("Hello");
+//
+// };
+//
+//   exports.NoteController = NoteController;
+// })(this);
+
+
+// var element = document.getElementById("app");
+// console.log(element);
+
+// element.innerHTML = "Bye";
+// console.log(element);
