3448d1f30cde143e39d3fe92ec5dc407c9d4d4c7
Lucien Najev <luciennajev@hotmail.com>
2018-09-19 21:02:49 +0100

    refactoring

--- src/note-controller.js
+++ src/note-controller.js
@@ -6,8 +6,8 @@
   };
 
   NoteController.prototype.displayNotes = function() {
-    // var view = new View(this.list);
     document.getElementById("app").innerHTML = this.view.displayHTML();
+    console.log(document.getElementById("app").innerHTML);
     return document.getElementById("app").innerHTML;
   };
 
