5e5a367981f2111668a1a927cd06f42b164c6c63
Lucien Najev <luciennajev@hotmail.com>
2018-09-19 15:46:17 +0100

    refactor

--- test/note-list-view-tests.js
+++ test/note-list-view-tests.js
@@ -3,12 +3,12 @@
   function viewTestSeveral() {
 
     var list = new List();
-    list.createAndStore("Favourite food: pesto");
+    list.createAndStore("Favourite food: pesto pasta");
     list.createAndStore("Favourite drink: seltzer");
     list.createAndStore("Favourite dessert: cake");
     var view = new View(list);
 
-    if(view.displayHTML() !== "<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li><li><div>Favourite dessert: cake</div></li></ul>") {
+    if(view.displayHTML() !== "<ul><li><div>Favourite food: pesto pasta</div></li><li><div>Favourite drink: seltzer</div></li><li><div>Favourite dessert: cake</div></li></ul>") {
       throw new Error("List does not return required test output");
     } else {
       console.log("Successful");
