4659c914f9d526f32d00e2ab2c03a3e730b7b543
Lucien Najev <luciennajev@hotmail.com>
2018-09-19 13:34:55 +0100

    test amended

--- src/note-controller.js
+++ src/note-controller.js
@@ -2,12 +2,13 @@
 
   function NoteController(list = new List()) {
     this.list = list;
+    this.view = new View(this.list);
   };
 
 
   NoteController.prototype.displayNotes = function() {
-    var view = new View(this.list);
-    document.getElementById("app").innerHTML = view.displayHTML();
+    // var view = new View(this.list);
+    document.getElementById("app").innerHTML = this.view.displayHTML();
     return document.getElementById("app").innerHTML;
   };
 
